487bbba7e25b1389d0dd5f6a4b682512e33b45ac
xuetao <gentle.taoxue@gmail.com>
2019-01-30 23:23:04 +0800

    Initial commit

---
+++ src/main/java/com/xt/configtool/ConfigtoolApplication.java
@@ -0,0 +1,14 @@
+package com.xt.configtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigtoolApplication.class, args);
+    }
+
+}
+
