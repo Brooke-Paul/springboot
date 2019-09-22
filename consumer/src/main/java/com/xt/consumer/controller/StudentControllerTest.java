a2ac24e9d36ac5b706d8711a547949ade958547e
xuetao <gentle.taoxue@gmail.com>
2019-09-22 14:55:28 +0800

    新增springboot sofaboot rpc 实现分布式服务

--- consumer/src/main/java/com/xt/consumer/controller/StudentControllerTest.java
+++ consumer/src/main/java/com/xt/consumer/controller/StudentControllerTest.java
@@ -1,7 +1,14 @@
 package com.xt.consumer.controller;
 
+import com.xt.api.entity.Studtent;
 import com.xt.api.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author xuetao
@@ -9,8 +16,25 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @Date 2019-02-14
  * @Version 1.0
  */
+@Controller
+@RequestMapping("index")
 public class StudentControllerTest {
 
     @Autowired
     private StudentService studentService;
+
+    @RequestMapping("find")
+    @ResponseBody
+    public Studtent getStudentByID(HttpServletRequest request) throws Exception {
+        Studtent studtent = studentService.findStudent();
+        return studtent;
+    }
+
+
+    @RequestMapping("findOne")
+    @ResponseBody
+    public Studtent getStudent(HttpServletRequest request) throws Exception {
+        Studtent studtent = studentService.findStudent();
+        return studtent;
+    }
 }
